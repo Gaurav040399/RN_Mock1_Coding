800ea4e9895c71c155503e7600f14b74f26e9140
Gaurav Asode <gauravasode5@gmail.com>
2023-08-07 15:21:03 +0530

    json Server create

---
+++ index.js
@@ -0,0 +1,19 @@
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
+require("dotenv").config()
+
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+
+
+server.listen(process.env.PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${process.env.PORT}`)
+})
